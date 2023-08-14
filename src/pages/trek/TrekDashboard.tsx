58c260e0c5f05a11c7ba7d9c786dd53a4a778c72
Bruno Assis Carvalho <brunoacarvalho@live.com>
2023-08-14 17:17:39 -0300

    prepare flow to api

--- src/pages/trek/TrekDashboard.tsx
+++ src/pages/trek/TrekDashboard.tsx
@@ -1,5 +1,5 @@
-import { Flow } from '@mern-monorepo/ui-react-template';
-import { useCallback } from 'react';
+import { Flow, FlowObject } from '@mern-monorepo/ui-react-template';
+import { useCallback, useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import 'reactflow/dist/style.css';
@@ -20,19 +20,41 @@ const intialFlow = [
 ];
 
 export default function TrekDashboard() {
-  let { idDetail } = useParams<string>();
   const navigate = useNavigate();
+  let { idDetail } = useParams<string>();
+  const [dataFlow, setDataFlow] = useState<FlowObject>();
 
   const onNodeDoubleClick = useCallback((event: React.MouseEvent, node: any) => {
     event.preventDefault();
     navigate(`new/${node.type}`);
   }, []);
 
+  const changeFlow = (data: FlowObject) => {
+    setDataFlow(data);
+    console.log('changedddd', data);
+  };
+
+  useEffect(() => {
+    let timeout = setTimeout(() => {
+      console.log('time', dataFlow);
+    }, 3000);
+
+    return () => {
+      console.log('end time');
+      clearTimeout(timeout);
+    };
+  }, [dataFlow]);
+
   return (
     <>
       <>Detail {idDetail}</>
       <div style={{ width: '90vw', height: '90vh' }}>
-        <Flow onNodeDoubleClick={onNodeDoubleClick} nodeTypes={CustomNodeList} flowData={intialFlow} />
+        <Flow
+          onNodeDoubleClick={onNodeDoubleClick}
+          nodeTypes={CustomNodeList}
+          flowData={intialFlow}
+          onChange={changeFlow}
+        />
       </div>
     </>
   );
